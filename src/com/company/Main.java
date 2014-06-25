0eca3e473cabba695cf9e238a18c4ff96a8ba314
Dillon <9eda@queensu.ca>
2014-06-25 15:09:24 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Random;
+
+public final class Main {
+    public static final void main(String... aArgs){
+        System.out.println("Creating 10 random integers between 1 and 100");
+
+        Random randomGenerator = new Random();
+        for (int i = 1; i <= 10; ++i){
+            int randomInt = randomGenerator.nextInt(100);
+            System.out.println("Random " + i + " : " + randomInt);
+        }
+
+        System.out.println("All numbers generated");
+    }
+
+}
+
