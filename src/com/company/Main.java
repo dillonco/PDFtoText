a68724759d52e65d1a905706097889e85549e7b2
Dillon <nixhatter@gmail.com>
2014-07-02 13:46:51 -0400

    Installed Tess4J

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,52 +1,20 @@
 package com.company;
 
-import java.util.*;
+import java.io.File;
+import net.sourceforge.tess4j.*;
 
-public final class Main {
-     public static void main (String[] args){
-       /* System.out.println("Creating 10 random integers between 1 and 100");
+public class Main {
 
-        Random randomGenerator = new Random();
-        for (int i = 1; i <= 10; ++i){
-            int randomInt = randomGenerator.nextInt(100);
-            System.out.println("Random " + i + " : " + randomInt);
+    public static void main(String[] args) {
+        File imageFile = new File("eurotext.tif");
+        Tesseract instance = Tesseract.getInstance();  // JNA Interface Mapping
+        // Tesseract1 instance = new Tesseract1(); // JNA Direct Mapping
+
+        try {
+            String result = instance.doOCR(imageFile);
+            System.out.println(result);
+        } catch (TesseractException e) {
+            System.err.println(e.getMessage());
         }
-
-        for (int i = 1; i <= 10; i++){
-            double randomInt2 = Math.floor(Math.random() * 100);
-            System.out.println("Random 2 " + i + ": " + randomInt2);
-        }
-
-        System.out.println("Monster Ranchers Double Swag");
-        Test
-        **/
-
-         Scanner sc = new Scanner(System.in);
-         for (int i = 1; i <= 5; i++) {
-             System.out.println("Please Enter First Name");
-             String firstName = sc.nextLine();
-             System.out.println("Please Enter Last Name");
-             String lastName = sc.nextLine();
-
-             Name name = new Name(firstName, lastName);
-
-             System.out.println("Please Enter Your House Number and Street Name");
-             String street = sc.nextLine();
-             System.out.println("Please Enter Your City Name");
-             String city = sc.nextLine();
-             System.out.println("Please Enter Your State/Province Name");
-             String state = sc.nextLine();
-             System.out.println("Please Enter Your ZIP Code");
-             String zip = sc.nextLine();
-
-             Address address = new Address(street, city, state, zip);
-
-             Person person = new Person(name, address, i);
-
-             System.out.println(person.toString());
-             System.out.println();
-         }
-
     }
-
 }
