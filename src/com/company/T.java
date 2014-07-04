07c896208f564b9eae44ff8eb6fd1dac03efc3f3
dowonp <pdowon@hotmail.com>
2014-07-03 22:36:37 -0400

    basicGUI

--- src/com/company/T.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-/**
- * Created by Denny on 7/3/2014.
- */
-public class T is imba {
-}
---
+++ src/com/company/basicGUI.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import javax.swing.*;
+
+/**
+ * Created by Denny on 7/3/2014.
+ */
+public class basicGUI {
+    private JButton closeButton;
+    private JTextField browseFileTextField;
+    private JTextField saveAsTextField;
+    private JButton browseButton;
+    private JButton saveAsButton;
+}
