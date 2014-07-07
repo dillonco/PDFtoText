dd0a94fbb827a8a6c37cf157edea24ad3cc13d08
dowonp <pdowon@hotmail.com>
2014-07-07 14:19:31 -0400

    Sync

--- src/com/company/basicGUI.java
+++ src/com/company/basicGUI.java
@@ -46,17 +46,17 @@ public class basicGUI extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 JFileChooser chooser = new JFileChooser();
                 chooser.setCurrentDirectory(new java.io.File("."));
-                chooser.setDialogTitle("Save As");
+                chooser.setDialogTitle("Save At");
                 chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
                 chooser.setAcceptAllFileFilterUsed(false);
 
                 if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-                    System.out.println("getCurrentDirectory(): " + chooser.getCurrentDirectory());
+
                     System.out.println("getSelectedFile() : " + chooser.getSelectedFile());
                 } else {
                     System.out.println("No Selection ");
                 }
-            
+
             }
                                        });
 
